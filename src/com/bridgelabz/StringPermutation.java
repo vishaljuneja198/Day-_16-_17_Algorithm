45e474f0506ebdfe5623f7b79b448668eef6ef24
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-26 16:04:15 +0530

    Added uc1 Permuation usin recusion

--- src/com/bridgelabz/StringPermutation.java
+++ src/com/bridgelabz/StringPermutation.java
@@ -1,7 +1,21 @@
-package com.bridgelabz;
+package com.bridgelabz.algorithms;
 
 public class StringPermutation {
+    static void permutate(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String ros = str.substring(0, i) + str.substring(i + 1);
+            permutate(ros, ans + ch);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("welcome to day 16 & 17 Algo programs");
+        String s = "abb";
+        permutate(s, "");
     }
 }
