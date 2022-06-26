5690669aee090bc4b43cbbb470e7450f333fa128
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-26 16:09:08 +0530

    Added uc2 Find word

--- src/com/bridgelabz/FindWord.java
+++ src/com/bridgelabz/FindWord.java
@@ -1,2 +1,23 @@
-package com.bridgelabz;public class FindWord {
+package com.bridgelabz.algorithms;
+
+import java.util.Scanner;
+
+public class FindWord {
+    public static void main(String[] args) {
+        String findWord[] = {"Ramesh", "Rajesh", "Mohit", "Rohit"};
+
+        System.out.println("Enter the word you want to find ");
+        String search = new Scanner(System.in).nextLine();
+        boolean flag = false;
+
+        for (String found : findWord) {
+            if (found.equals(search)) {
+                System.out.println(found + " has been found");
+                flag = true;
+                break;
+            }
+        }
+        if(flag==false)
+            System.out.println(search +" not found");
+    }
 }
