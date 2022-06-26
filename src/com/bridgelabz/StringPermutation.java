efa5aa8fc6027eeec2a4624b74d19f0da5f9b1ee
vishaljuneja198 <vishaljuneja198@gmail.com>
2022-06-26 16:01:42 +0530

    Welcome message of day 16 & 17

---
+++ src/com/bridgelabz/StringPermutation.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class StringPermutation {
+    public static void main(String[] args) {
+        System.out.println("welcome to day 16 & 17 Algo programs");
+    }
+}
